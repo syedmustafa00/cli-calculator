21e07f319cf75e0cc094e2fdb8e7a355eb4aca77
syedmustafa00 <165793530+syedmustafa00@users.noreply.github.com>
2024-04-05 18:15:42 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,45 @@
+#! /usr/bin/env node
+import { log } from "console";
+
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+{message: "enter your first number",type: "number",name: "firstnumber"},
+{message: "enter second your number", type: "number", name: "secondnumber"},
+{
+  message: "select one of the operator to perform operation",
+    type: "list",
+    name: "operator",
+    choices: [ "addition", "subtraction", "multiplication", "division", ]
+},
+]);
+
+//conditional statement
+
+if
+(answer.operator === "addition") {
+console.log(answer.firstnumber + answer.secondnumber)
+}
+ else if
+ (answer.operator === "subtraction")
+
+ {console.log(answer.firstnumber - answer.secondnumber);
+}
+ else if
+ (answer.operator === "multiplication")
+{console.log(answer.firstnumber * answer.secondnumber);
+}
+else if
+(answer.operator === "division")
+{console.log((answer.firstnumber / answer.secondnumber).toFixed(4));
+}
+
+
+
+
+
+
+
+
+
+
